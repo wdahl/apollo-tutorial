c42baf298e453043c6a5c72c6269a7d3185a8634
Will Dahl <wdahl@albany.edu>
2021-01-21 17:00:40 -0500

    client config and source

---
+++ client/src/containers/logout-button.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'react-emotion'
+import { useApolloClient } from '@apollo/client'
+
+import { menuItemClassName } from '../components/menu-item'
+import { isLoggedInVar } from '../cache'
+import { ReactComponent as ExitIcon } from '../assets/icons/exit.svg'
+
+const LogoutButton = () => {
+  const client = useApolloClient();
+  return (
+    <StyledButton data-testid="logout-button" onClick={() => {
+      client.cache.evict({ fieldName: 'me '});
+      client.cache.gc();
+
+      localStorage.removeItem('token');
+      localStorage.removeItem('userId');
+
+      isLoggedInVar(false);
+    }}>
+      <ExitIcon />
+      Logout
+    </StyledButton>
+  );
+}
+
+export default LogoutButton;
+
+const StyledButton = styled('button')(menuItemClassName, {
+  background: 'none',
+  border: 'none',
+  padding: 0
+})
---
+++ client/src/index.tsx
@@ -0,0 +1,49 @@
+import {
+    ApolloClient,
+    NormalizedCacheObject,
+    ApolloProvider,
+    gql,
+    useQuery
+} from '@apollo/client'
+import Login from './pages/login'
+import { cache } from './cache'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Pages from './pages'
+import injectStyles from './styles'
+
+const IS_LOGGED_IN = gql`
+    query IsUserLoggedIn {
+        isLoggedIn @client
+    }
+`;
+
+function IsLoggedIn(){
+    const { data } = useQuery(IS_LOGGED_IN)
+    return data.isLoggedIn ? <Pages /> : <Login />
+}
+
+export const typeDefs = gql`
+    extend type Query {
+        isLoggedIn: Boolean!
+        cartItems: [ID!]!
+    }
+`;
+
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+    cache,
+    uri: 'http://localhost:4000/graphql',
+    headers: {
+        authorization: localStorage.getItem('token') || ''
+    },
+    typeDefs
+})
+
+injectStyles()
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <IsLoggedIn />
+    </ApolloProvider>,
+    document.getElementById('root')
+)
